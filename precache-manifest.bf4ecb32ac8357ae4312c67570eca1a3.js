89e10bf82054b5255745dd8fc741b08b335b2cda
Vishal Roy <vishalroyfiitjee1998@gmail.com>
2020-09-26 11:00:58 +0530

    Updates

---
+++ precache-manifest.bf4ecb32ac8357ae4312c67570eca1a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d8d48d8ae6f09ef26fb12df9ef574e",
+    "url": "/movie-app-2019/index.html"
+  },
+  {
+    "revision": "0ff4ab54e746d2a530c8",
+    "url": "/movie-app-2019/static/js/2.d39a9333.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app-2019/static/js/2.d39a9333.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0315525682d337e3fa0",
+    "url": "/movie-app-2019/static/js/main.71f17c2f.chunk.js"
+  },
+  {
+    "revision": "eb08383de6f66a64bb1e",
+    "url": "/movie-app-2019/static/js/runtime-main.80335faf.js"
+  }
+]);
